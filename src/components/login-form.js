77de0e34d7189266b0a6e7198c45b1e9e02a62a0
andreadsw <andrea.dossantosw@gmail.com>
2019-06-02 20:16:16 +0100

    Removing API creditials

--- src/components/login-form.js
+++ src/components/login-form.js
@@ -11,12 +11,12 @@ class LoginForm extends Component {
   componentDidMount() {
     // Adding here your Firebase API configuration.
     const config = {
-      apiKey: "AIzaSyBFgAJQ3OuBL8L37kGyJ1BaCF20vcULybc",
-      authDomain: "app-react-native-auth.firebaseapp.com",
-      databaseURL: "https://app-react-native-auth.firebaseio.com",
-      projectId: "app-react-native-auth",
-      storageBucket: "app-react-native-auth.appspot.com",
-      messagingSenderId: "957596588375"
+      apiKey: "",
+      authDomain: "",
+      databaseURL: "",
+      projectId: "",
+      storageBucket: "",
+      messagingSenderId: ""
     };
     firebase.initializeApp(config);
   }
